cf924d709216a25fba9cefdfa386dd0ddaed76c3
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2022-05-23 11:55:05 +0200

    refactor(epo_crawler): adapt format of id input param
    pass number instead of EPO id
    
    Co-authored-by: Clara Uktar <ClaraUktar@users.noreply.github.com>

--- epo_crawler/main.py
+++ epo_crawler/main.py
@@ -1,7 +1,6 @@
 import click
 import logging
 import os
-import re
 
 from epo_crawler.epo_extractor import EpoExtractor
 from epo_crawler.epo_parser import EpoParser
@@ -15,39 +14,29 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def validate_id(ctx, param, value):
-    if re.match("^EP[0-9]{6}[1-9]", value):
-        return value
-    else:
-        raise click.BadParameter(
-            "Parameter --id must be in format EPNNNNNNN where N is a digit between 0 and 9.")
-
-
 @click.command()
-@click.option("-i", "--id", required=True, callback=validate_id, help="The patent ID (publication) to initialize the crawl from (format: EPNNNNNNN where N is a digit between 0 and 9)")
-def run(id: str):
+@click.option("-i", "--id", required=True, type=click.IntRange(1, 9999999), help="The patent ID (publication) to initialize the crawl from (number between 1 and 9999999)")
+def run(id: int):
     load_dotenv()
     extractor = EpoExtractor()
     parser = EpoParser()
     producer = EpoProducer()
 
-    id_num = int(id[2:])
-
     while True:
-        id = f"EP{str(id_num).zfill(7)}"
-        logger.info(f"Fetching EPO data of publication with id {id}")
+        epo_id = f"EP{str(id).zfill(7)}"
+        logger.info(f"Fetching EPO data of publication with id {epo_id}")
 
-        epo_json = extractor.fetch(id)
+        epo_json = extractor.fetch(epo_id)
 
         if not epo_json:
             continue
 
-        patent = parser.serialize(id, epo_json)
+        patent = parser.serialize(epo_id, epo_json)
 
         producer.produce_to_topic(patent)
 
         sleep(0.5)
-        id_num += 1
+        id += 1
 
 
 if __name__ == "__main__":
