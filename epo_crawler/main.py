12b5ad676f80305325dbc54ed959597ba5db51fe
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2022-05-23 12:18:42 +0200

    feat(epo_crawler): add single option for CLI
    add single option which allows crawling a single entry (useful for
    debugging)

--- epo_crawler/main.py
+++ epo_crawler/main.py
@@ -16,7 +16,8 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.option("-i", "--id", required=True, type=click.IntRange(1, 9999999), help="The patent ID (publication) to initialize the crawl from (number between 1 and 9999999)")
-def run(id: int):
+@click.option("-s", "--single", is_flag=True, default=False, help="Whether a single patent should be crawled. Useful for debugging.")
+def run(id: int, single: bool):
     load_dotenv()
     extractor = EpoExtractor()
     parser = EpoParser()
@@ -28,12 +29,21 @@ def run(id: int):
 
         epo_json = extractor.fetch(epo_id)
 
-        if not epo_json:
+        if single:
+            logger.debug(epo_json)
+
+        if not single and not epo_json:
             continue
 
         patent = parser.serialize(epo_id, epo_json)
 
+        if single:
+            logger.debug(patent)
+
         producer.produce_to_topic(patent)
+
+        if single:
+            break
 
         sleep(0.5)
         id += 1
