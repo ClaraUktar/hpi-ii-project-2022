47f2bae7c046590c93df66860c115e9aeb5666f4
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2022-05-25 14:26:11 +0200

    feat(connect): adapt config for corporate & patent connectors
    add elastic sink config for EPO
    
     adapt config scripts to handle both
    sink config files

--- connect/delete-config.sh
+++ connect/delete-config.sh
@@ -2,8 +2,14 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
+PATENT_CONFIG=${4:-"$(dirname $0)/elastic-sink-patent.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-CONNECTOR_NAME=$(jq -r .name $BASE_CONFIG)
+# Remove corporate events connector
+CONNECTOR_NAME=$(jq -r .name $CORPORATE_CONFIG)
+curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
+
+# Remove patent events connector
+CONNECTOR_NAME=$(jq -r .name $PATENT_CONFIG)
 curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
--- connect/push-config.sh
+++ connect/push-config.sh
@@ -2,8 +2,14 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
+PATENT_CONFIG=${4:-"$(dirname $0)/elastic-sink-patent.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-data=$(cat $BASE_CONFIG | jq -s '.[0]')
+# Create corporate events connector
+data=$(cat $CORPORATE_CONFIG | jq -s '.[0]')
+curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
+
+# Create patent events connector
+data=$(cat $PATENT_CONFIG | jq -s '.[0]')
 curl -X POST $KAFKA_CONNECT_API --data "$data" -H "content-type:application/json"
