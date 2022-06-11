86a777a5315aa8c8a73672639594f29538264c9b
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2022-06-11 19:44:37 +0200

    feat: adapt config to use Neo4j as database
    add Neo4j to docker compose
    ignore Elasticsearch in docker compose
    add Neo4j Kafka Connect sinks
    adapt EPO protobuf schema
    
    Co-authored-by: Clara Uktar <ClaraUktar@users.noreply.github.com>

--- connect/push-config.sh
+++ connect/push-config.sh
@@ -2,8 +2,10 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
-PATENT_CONFIG=${4:-"$(dirname $0)/elastic-sink-patent.json"}
+# CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic-sink-corporate.json"}
+# PATENT_CONFIG=${4:-"$(dirname $0)/elastic-sink-patent.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/neo4j-sink-announcements.json"}
+PATENT_CONFIG=${4:-"$(dirname $0)/neo4j-sink-patents.json"}
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
 # Create corporate events connector
